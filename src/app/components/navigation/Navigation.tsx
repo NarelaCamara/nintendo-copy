c780274c6f314c73933a17b8f549b63cb7f64bdb
RoxanaCamara <roxana.camara07@gmail.com>
2024-11-26 14:55:54 -0300

    feat: search component

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import React from "react";
 import profilePic from "/public/Nintendo.svg.png";
+import SearchComponent from "../search_component/search_component";
 
 const Navigation = () => {
   return (
-    <div className="">
-      <Image src={profilePic} alt="Icono nintendo" />
+    <div className="grid grid-cols-3 gap-1 bg-fuchsia-50">
+      <div className="w-40">
+        <Image src={profilePic} alt="Icono nintendo" />
+      </div>
+      <div className="">
+        <SearchComponent />
+      </div>
     </div>
   );
 };
