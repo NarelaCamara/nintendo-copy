e4be65bfcea41a870870ce403ffb606cb3274ca7
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-12 12:39:40 -0300

    feat: search

--- src/app/components/search_component/search_component.tsx
+++ src/app/components/search_component/search_component.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MagnifyingGlassIcon,
-  ChevronUpIcon,
   ChevronDownIcon,
 } from "@heroicons/react/24/solid";
 
 const SearchComponent = () => {
+  const [changeChevron, setChangeChevron] = useState(true);
   return (
-    <div>
-      <MagnifyingGlassIcon className="size-6 text-stone-900" />
+    <div className="grid grid-cols-3 bg-slate-500 w-64 ">
+      <div>
+        <MagnifyingGlassIcon className="size-6 text-stone-900" />
+      </div>
 
-      <ChevronUpIcon className="size-6 text-stone-900" />
-
-      <ChevronDownIcon className="size-6 text-stone-900" />
+      <div
+        className="absolute  border-black w-60 m-4 "
+        onClick={() => setChangeChevron(!changeChevron)}
+      >
+        <ChevronDownIcon
+          className={`   ${
+            changeChevron
+              ? "duration-300 transform rotate-180"
+              : " duration-300 transform "
+          } size-6 text-stone-900`}
+        />
+      </div>
     </div>
   );
 };
