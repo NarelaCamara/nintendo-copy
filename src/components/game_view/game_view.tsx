4f7b48f34b155709cdb7aaff5f813a19f37a9a3f
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-13 17:04:21 -0300

    feat: some text

--- src/components/game_view/game_view.tsx
+++ src/components/game_view/game_view.tsx
@@ -8,15 +8,32 @@ import RecentlyViewed from "../recently_viewed/recently_viewed";
 
 const GameView = () => {
   return (
-    <div>
+    <div className="w-2/3 mx-auto max-w-screen-lg">
       <CardGame />
       <DescriptionGame />
-      <div>
-        <ESRB_Rating />
-        <SupportedPlayModesGame />
+      <div className="flex flex-row justify-around">
+        <div className="p-4">
+          <ESRB_Rating />
+        </div>
+
+        <div className="p-4">
+          <SupportedPlayModesGame />
+        </div>
       </div>
       <CharacteristicsGame />
       <RecentlyViewed />
+
+      <p>
+        WARNING: If you have epilepsy or have had seizures or other unusual
+        reactions to flashing lights or patterns, consult a doctor before
+        playing video games. All users should read the Health and Safety
+        Information available in the system settings before using this software.
+        Nintendo Switch Online membership (sold separately) and Nintendo Account
+        required for online play. Not available in all countries. Internet
+        access required for online features. Terms apply.
+        nintendo.com/switch-online Stardew Valley © ConcernedApe LLC 2016-2020.
+        All rights reserved.
+      </p>
     </div>
   );
 };
