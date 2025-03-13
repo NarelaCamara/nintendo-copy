0afab61eef29764ad5ce2f1c75c64918ac277469
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-13 17:03:47 -0300

    feat: add text

--- src/components/card_game/card_game.tsx
+++ src/components/card_game/card_game.tsx
@@ -18,10 +18,15 @@ const CardGame = () => {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900  ">
               Stardew Valley
             </h5>
+            <p className="text-gray-700">Eligible for up to 75 Gold Points</p>
             <p className="mb-3 font-normal text-gray-700  ">$14.99</p>
             <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300    ">
               Direct download
             </button>
+            <p className="text-gray-700">
+              This item will be sent to your system automatically after
+              purchase.
+            </p>
           </div>
         </div>
       </div>
