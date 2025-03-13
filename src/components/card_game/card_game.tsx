fdee7ab6966ad802c1fb29dd6b30994dd9f9723e
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-13 13:59:03 -0300

    feat: add componete card

--- src/components/card_game/card_game.tsx
+++ src/components/card_game/card_game.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
 const CardGame = () => {
-  return <div>CardGame</div>;
+  return (
+    <div>
+      <div className=" m-4 p-6  bg-slate-200 border border-gray-800 rounded-lg shadow-sm    ">
+        <div className="flex flex-row">
+          <div className="p-4">
+            <Image
+              src="/stardew_valley.avif"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="p-4">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900  ">
+              Stardew Valley
+            </h5>
+            <p className="mb-3 font-normal text-gray-700  ">$14.99</p>
+            <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300    ">
+              Direct download
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardGame;
