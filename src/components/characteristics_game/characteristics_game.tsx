992f9bcef344a4332e999bae456f014e71b7e99d
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-13 17:05:17 -0300

    feat: characteristic

--- src/components/characteristics_game/characteristics_game.tsx
+++ src/components/characteristics_game/characteristics_game.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const CharacteristicsGame = () => {
-  return <div>CharacteristicsGame</div>;
+  return (
+    <div className=" bg-slate-400">
+      <div className="flex flex-column justify-around">
+        <p>Release date</p>
+        <p>October 5, 2017</p>
+      </div>
+
+      <div className="p-4">
+        <hr className="  h-px my-8 bg-gray-200 border-0  " />
+      </div>
+    </div>
+  );
 };
 
 export default CharacteristicsGame;
