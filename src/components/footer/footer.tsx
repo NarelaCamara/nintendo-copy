bf26f9ba30d5bc279fb0b4efb4a4e83b5b198b30
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-13 12:08:46 -0300

    feat: footer last

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,6 +5,19 @@ const Footer = () => {
   return (
     <div>
       <FooterNavigation />
+
+      <div className="flex flex-row justify-center">
+        <p className="px-8">
+          © Nintendo. Games are property of their respective owners. Nintendo of
+          America Inc. Headquarters are in Redmond, Washington, USA
+        </p>
+        <div className="flex flex-row justify-center px-8">
+          <p className="px-4">Contact us</p>
+          <p className="px-4">Website feedback</p>
+          <p className="px-4">Terms of Use</p>
+          <p className="px-4">Flag of Usa English (United States)</p>
+        </div>
+      </div>
     </div>
   );
 };
