6d1fe22eb314b1e006feb2f015c30b69722a57c0
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-13 12:20:54 -0300

    feat: publicity

--- src/components/publicity/publicity.tsx
+++ src/components/publicity/publicity.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Publicity = () => {
-  return <div> </div>;
+  return (
+    <div className="flex flex-row justify-center bg-slate-600">
+      <p className="p-4">
+        Free shipping on orders $50 or more. Restrictions apply. Earn My
+        Nintendo
+      </p>
+      <p className="p-4"> Points on digital games</p>
+    </div>
+  );
 };
 
 export default Publicity;
