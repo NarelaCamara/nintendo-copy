3cf907752dc8bcce08d3d1389864d130501c6b8a
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-13 17:04:55 -0300

    feat: add border

--- src/components/esrb_rating/esrb_rating.tsx
+++ src/components/esrb_rating/esrb_rating.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ESRB_Rating = () => {
-  return <div>ESRB_Rating</div>;
+  return (
+    <div className=" border border-gray-800 rounded-lg shadow-sm ">
+      ESRB_Rating
+    </div>
+  );
 };
 
 export default ESRB_Rating;
--- src/components/supported_play_modes_game/supported_play_modes_game.tsx
+++ src/components/supported_play_modes_game/supported_play_modes_game.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SupportedPlayModesGame = () => {
-  return <div>SupportedPlayModesGame</div>;
+  return (
+    <div className=" border border-gray-800 rounded-lg shadow-sm ">
+      SupportedPlayModesGame
+    </div>
+  );
 };
 
 export default SupportedPlayModesGame;
