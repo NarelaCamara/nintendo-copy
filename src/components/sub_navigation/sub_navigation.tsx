4472e5dd84305a494bbb30871a801e67bc0f7943
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-13 12:09:17 -0300

    feat: subNav

--- src/components/sub_navigation/sub_navigation.tsx
+++ src/components/sub_navigation/sub_navigation.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const SubNavigation = () => {
   return (
-    <div>
-      <h4>My Nintendo store</h4>
-      <h4>Games</h4>
-      <h4>Nintendo Switch</h4>
-      <h4>New&apos;s & events</h4>
-      <h4>Play Nintendo</h4>
+    <div className="flex flex-row justify-center">
+      <h4 className="p-4">My Nintendo store</h4>
+      <h4 className="p-4">Games</h4>
+      <h4 className="p-4">Nintendo Switch</h4>
+      <h4 className="p-4">New&apos;s & events</h4>
+      <h4 className="p-4">Play Nintendo</h4>
     </div>
   );
 };
