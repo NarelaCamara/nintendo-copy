8b0efec5d120c5b46d18923abe5719195c40ecbd
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-16 20:21:55 -0300

    feat: add footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,41 @@
 import React from "react";
 import FooterNavigation from "../footer_navigation/footer_navigation";
+import Image from "next/image";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/solid";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="">
+      <div className="bg-red-600">
+        <div className="flex flex-row justify-between ">
+          <Image
+            width={250}
+            height={250}
+            src={"/Nintendo.svg.png"}
+            alt="Icono nintendo"
+          />
+
+          <div className="flex flex-row">
+            <div className="p-2">
+              <QuestionMarkCircleIcon className="size-6 text-stone-900" />
+            </div>
+            <div className="p-2">
+              <QuestionMarkCircleIcon className="size-6 text-stone-900" />
+            </div>
+            <div className="p-2">
+              <QuestionMarkCircleIcon className="size-6 text-stone-900" />
+            </div>
+            <div className="p-2">
+              <QuestionMarkCircleIcon className="size-6 text-stone-900" />
+            </div>
+          </div>
+        </div>
+      </div>
+
       <FooterNavigation />
 
-      <div className="flex flex-row justify-center">
-        <p className="px-8">
+      <div className="w-3/5 mx-auto max-w-screen-lg ">
+        <p className="px-8 ">
           © Nintendo. Games are property of their respective owners. Nintendo of
           America Inc. Headquarters are in Redmond, Washington, USA
         </p>
@@ -18,6 +46,7 @@ const Footer = () => {
           <p className="px-4">Flag of Usa English (United States)</p>
         </div>
       </div>
+      
     </div>
   );
 };
